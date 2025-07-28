fd6f469ccadbc6459e577d4b70cb841eff6739e1
himel2535 <monwarhossanhimel@gmail.com>
2025-07-28 23:57:04 +0600

    added learning logical-not

---
+++ logical-not.js
@@ -0,0 +1,13 @@
+let price = 500;
+const isLeader=true;
+
+//eikhane boolian==true na likhe just boolian likhelo hbe.
+//jodi false bujhate cai thle just samne ekta exclamatory sign dilei hbe (!)
+
+if(!isLeader){
+    price=0;
+}
+else{
+    price=price*2;
+}
+console.log(price)
