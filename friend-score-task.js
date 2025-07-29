e8ec43bf4c2afea5f96b66af0018812cbdeae782
himel2535 <monwarhossanhimel@gmail.com>
2025-07-29 15:20:11 +0600

    doing friend-score-task

---
+++ friend-score-task.js
@@ -0,0 +1,19 @@
+let myScore = 70;
+let friendScore = 89;
+if (myScore>=80){
+    if(friendScore >= 80){
+        console.log('go for a lunch')
+    }
+    else if( friendScore>=60){
+        console.log('good luck , next time')
+    }
+    else if( friendScore >=40){
+        console.log('massage unseen')
+    }
+    else{
+        console.log('block him')
+    }
+}
+else{
+    console.log('got home,sleep and act sad')
+}
