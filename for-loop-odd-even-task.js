9b8d3253179255c64aeeb9c9ef20fb68a34e3aae
himel2535 <monwarhossanhimel@gmail.com>
2025-07-30 17:57:54 +0600

    even task done

--- for-loop-odd-even-task.js
+++ for-loop-odd-even-task.js
@@ -1,9 +1,18 @@
+// let sum=0;
+// for(let i=61;i<=100;i++){
+//     if(i%2===1){
+//         sum=sum+i;
+//         // console.log(sum)
+//         console.log(i)
+//     }
+// }
+// console.log("sum of odd number : " ,sum);
+
 let sum=0;
-for(let i=0;i<=10;i++){
-    if(i%2===1){
-        sum=sum+i;
-        // console.log(sum)
+for (let i=78 ; i<=98 ; i++){
+    if (i%2===0){
         console.log(i)
+        sum=sum+i;
     }
 }
-console.log("sum of odd number : " ,sum);
+console.log("the sum of even ",sum)
