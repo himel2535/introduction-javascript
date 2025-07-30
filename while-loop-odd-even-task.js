6118c817a048f57eb20fd9f4d0f843e5547dc2af
himel2535 <monwarhossanhimel@gmail.com>
2025-07-30 16:11:55 +0600

    odd-even by while loop

---
+++ while-loop-odd-even-task.js
@@ -0,0 +1,15 @@
+let i=61;
+while(i<=100){
+    if (i%2===1){
+        console.log("odd number is :" ,i)
+    }
+    i++;
+}
+
+let even=78;
+while(even<=98){
+    if (even%2===0){
+        console.log("even number is :",even)
+    }
+    even++;
+}
