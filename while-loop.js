506f714700a7ca0b3624744465697fc41376d825
himel2535 <monwarhossanhimel@gmail.com>
2025-07-30 16:04:02 +0600

    learning while loop and complete task1

---
+++ while-loop.js
@@ -0,0 +1,21 @@
+// let num=0;
+// while(num<=10){
+//     console.log(num)
+//     num++;
+// }
+
+// let i=0;
+// while(i<=10){
+//     if (i%2===0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+let i=0;
+while (i<=10){
+    if (i%2===1){
+      console.log("odd number is :",i)  
+    }
+    i++;
+}
