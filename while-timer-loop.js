1b8303f54c081d19935ee16d7691a51278c273ab
himel2535 <monwarhossanhimel@gmail.com>
2025-07-30 17:38:49 +0600

    count down solution task

---
+++ while-timer-loop.js
@@ -0,0 +1,15 @@
+// let count=81;
+// let timer = setInterval(() => {
+//     console.log(count);
+//     count--;
+//     if (count<65){
+//         clearInterval(timer);
+//     }
+    
+// }, 1000);
+
+let count=81;
+while(count >= 65){
+    console.log(count);
+    count-- ;
+}
