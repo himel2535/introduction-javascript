bcf7731c630ed19ca5dfbf15c98a32240ab3d4ff
himel2535 <monwarhossanhimel@gmail.com>
2025-07-30 18:56:12 +0600

    learn break and continue

---
+++ continue.js
@@ -0,0 +1,17 @@
+// for (let i=0;i<=10;i++){
+//     if(i>=5){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+let i=0;
+while(i<=10){
+    i++;
+    if(i%2!==0){
+        continue;
+    }
+    console.log(i)
+    
+    
+}
