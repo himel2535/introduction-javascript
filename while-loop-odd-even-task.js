c0d078a304a85ad319631b7160aff750806833f1
himel2535 <monwarhossanhimel@gmail.com>
2025-07-30 16:37:33 +0600

    complete odd even task using while loop

--- while-loop-odd-even-task.js
+++ while-loop-odd-even-task.js
@@ -1,15 +1,35 @@
-let i=61;
-while(i<=100){
-    if (i%2===1){
-        console.log("odd number is :" ,i)
+// let i=61;
+// while(i<=100){
+//     if (i%2===1){
+//         console.log("odd number is :" ,i)
+//     }
+//     i++;
+// }
+
+// let even=78;
+// while(even<=98){
+//     if (even%2===0){
+//         console.log("even number is :",even)
+//     }
+//     even++;
+// }
+
+// let i=91;
+// let sum=0;
+// while(i<=129){
+//     if(i%2===1){     
+//         sum=sum+i;
+//     }
+//     i++;
+// }
+// console.log("the sum is : " ,sum);
+
+let i=51;
+let sum=0;
+while(i<=85){
+    if (i%2===0){
+        sum=sum+i;
     }
     i++;
 }
-
-let even=78;
-while(even<=98){
-    if (even%2===0){
-        console.log("even number is :",even)
-    }
-    even++;
-}
+console.log("the sum is : " ,sum);
