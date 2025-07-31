0370990c5491b2b5846cc8f254f47172c45e3da3
himel2535 <monwarhossanhimel@gmail.com>
2025-07-31 21:43:27 +0600

    declare array

---
+++ declare-array.js
@@ -0,0 +1,5 @@
+let fruits=['mango','banana','pineapple','watermelon','orange']
+console.log('original array : ',fruits)
+console.log('print only 3rd element of the array:',fruits[2])
+fruits[1]='jambura';
+console.log('after change the 2nd element',fruits)
