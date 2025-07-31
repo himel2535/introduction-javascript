321aa5984aff8fd43f5b9baf968dbb679a20b1f8
himel2535 <monwarhossanhimel@gmail.com>
2025-07-31 23:00:07 +0600

    learning about includes

---
+++ array-includes.js
@@ -0,0 +1,15 @@
+let family=['abba','amma','ami','vai','bon']
+console.log(family)
+console.log(family.includes('bou'))
+console.log(family.includes('ami'))
+
+
+
+let friends=['himel','jafar','ataor','mahdi','adib','nijhum']
+
+if(friends.includes('nijhum')){
+    console.log('i will never go on this spot')
+}
+else{
+    console.log('as ur wish as well as')
+}
