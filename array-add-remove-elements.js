fa3d301a2e6805d7018c8a20702d9d42c2a08a88
himel2535 <monwarhossanhimel@gmail.com>
2025-07-31 21:57:47 +0600

    array push-pop of elements

---
+++ array-add-remove-elements.js
@@ -0,0 +1,8 @@
+let touristSpot=['cox-bazar','st-martin','bandorban']
+console.log('we will go to:',touristSpot)
+touristSpot.push('khangrachori')
+console.log('after adding new spot:',touristSpot)
+touristSpot.push('kaptai-lake','shuvolong-hill')
+console.log('after adding another two spot:',touristSpot)
+touristSpot.pop()
+console.log(' after removing last element:',touristSpot)
