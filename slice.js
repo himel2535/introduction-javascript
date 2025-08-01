a8770ee6f80ef01e33ad452c02aacdb0626155b7
himel2535 <monwarhossanhimel@gmail.com>
2025-08-02 00:04:55 +0600

    learn about splite

--- slice.js
+++ slice.js
@@ -1,3 +1,16 @@
+// slice-->
 const address='andorkilla'
 const part = address.slice(2,5);
 console.log(part);
+
+//splite-->
+
+const sentence=" i am bangldeshi and bangali "
+console.log(sentence.split(' '))
+
+
+const s=" i am bangldeshi and bangali "
+console.log(s.split('b'))
+
+const fname= "rahim,karim,fahim,jarim"
+console.log(fname.split(','))
