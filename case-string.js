725128161cff2ed22119e4efa6c0fac85d1610d2
himel2535 <monwarhossanhimel@gmail.com>
2025-08-01 23:53:26 +0600

    learn about trim

--- case-string.js
+++ case-string.js
@@ -1,8 +1,17 @@
-let capital='dhaka'
-let myDivision='DhAka'
-if(capital.toLowerCase==myDivision.toLowerCase){
-    console.log("i'm lucky")
+// let capital='dhaka'
+// let myDivision='DhAka'
+// if(capital.toLowerCase==myDivision.toLowerCase){
+//     console.log("i'm lucky")
+// }
+// else{
+//     console.log("i'm unhappy")
+// }
+
+let drink='waTer'
+let liquid='  Water     '
+if(drink.trim().toLowerCase() === liquid.trim().toLowerCase()){
+    console.log('panir opor name jibon')
 }
-else{
-    console.log("i'm unhappy")
+else {
+    console.log('pani jokhon lal-neel')
 }
