5bae1967174b6c51984b6f9cf6e84d4c8d17d12e
himel2535 <monwarhossanhimel@gmail.com>
2025-08-02 22:40:00 +0600

    about object

---
+++ object.js
@@ -0,0 +1,18 @@
+const bottle = {
+    brand:'apple',
+    price: 45 ,
+    color: 'white',
+    idClean: false
+}
+const subject={
+    name:'biology',
+    teacher :'rasheda mam',
+    examDate:'30 feb',
+    chapters:['first','second','third'],
+    exams:{
+        name:'final exam',
+        marks:100
+    }
+}
+console.log(bottle)
+console.log(subject)
