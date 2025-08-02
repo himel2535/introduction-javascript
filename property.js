20b57b9b1421aded88677a3e9a239672dab93a5e
himel2535 <monwarhossanhimel@gmail.com>
2025-08-02 22:46:29 +0600

    property declaration by dot

---
+++ property.js
@@ -0,0 +1,15 @@
+const person = {
+    name : 'sodor mia',
+    age : 25,
+    proffession : 'developr',
+    salary : 25000,
+    married : true
+}
+
+// dot-notation-->
+
+console.log (person.proffession);
+const income = person.salary;
+console.log(income)
+
+
