e80e908b14d261cc14986dbc538f8db6480d54a0
himel2535 <monwarhossanhimel@gmail.com>
2025-08-03 18:00:05 +0600

    checking all vowels in a text

--- string-task3.js
+++ string-task3.js
@@ -1,1 +1,10 @@
 
+const text = "A quick brown fox jumps over the lazy dog";
+const vowels=['a','e','i','o','u'];
+let hasAll = true;
+for(i=0;i<vowels.length;i++){
+    if(!text.includes(vowels[i])){
+        hasAll=false;
+    }
+}
+console.log(hasAll)
