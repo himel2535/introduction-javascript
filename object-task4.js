71eae78780652a6b6386be17e5a8aea87442d9ab
himel2535 <monwarhossanhimel@gmail.com>
2025-08-03 13:21:14 +0600

    get length of object by properties

---
+++ object-task4.js
@@ -0,0 +1,12 @@
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+let keys = Object.keys(student)
+// for (key in keys){
+    
+// }
+console.log(keys.length)
+// console.log(student)
