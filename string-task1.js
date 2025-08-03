5cc7243cd3eaf070229f35611b387b3311018205
himel2535 <monwarhossanhimel@gmail.com>
2025-08-03 16:36:45 +0600

    get how many times a letter appears here

---
+++ string-task1.js
@@ -0,0 +1,18 @@
+let text='abbasia khilafat';
+let count=0;
+for (i=0;i<text.length;i++){
+    if(text[i]==='a'){
+        count++;
+    }
+}
+console.log(count)
+
+
+let sentence="mojadar khabar";
+c=0;
+for(i=0;i<sentence.length;i++){
+    if(sentence[i]==='a'){
+        c++;
+    }
+}
+console.log('This letter appears',c,'times here')
