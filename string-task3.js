c7103ca0eef312d21445d457ee8abd5bf7cd7c2d
himel2535 <monwarhossanhimel@gmail.com>
2025-08-03 18:02:21 +0600

    modify this string-task3

--- string-task3.js
+++ string-task3.js
@@ -1,9 +1,10 @@
 
 const text = "A quick brown fox jumps over the lazy dog";
 const vowels=['a','e','i','o','u'];
+lText=text.toLowerCase()
 let hasAll = true;
 for(i=0;i<vowels.length;i++){
-    if(!text.includes(vowels[i])){
+    if(!lText.includes(vowels[i])){
         hasAll=false;
     }
 }
